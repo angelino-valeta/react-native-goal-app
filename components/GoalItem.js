3d53b399c51a32d9dfabc47827dffbc12e54b41d
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-09 22:03:47 +0100

    add effect on pressed item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,15 @@
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
     <View style={styles.goalItem}>
-      <Text style={{ color: "white" }}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
+        <Text style={{ color: "white", padding: 8 }}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,10 +17,12 @@ function GoalItem(props) {
 export default GoalItem;
 
 const styles = StyleSheet.create({
-	goalItem: {
+  goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
-    backgroundColor: '#5e0acc',
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5
   }
-})
+});
