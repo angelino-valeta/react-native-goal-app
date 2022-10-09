8c8a422e041cb235852f541b2da022e8d7d2743d
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-09 22:04:22 +0100

    format code

--- App.js
+++ App.js
@@ -17,12 +17,22 @@ export default function App() {
 
   }
 
+  function deleteGoalHandler(id){
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id)
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text }/>
+          return <GoalItem 
+          text={itemData.item.text } 
+          id={itemData.item.id}
+          onDeleteItem={deleteGoalHandler}
+          />
         }} keyExtractor={(item, index) => {
           return item.id
         }} alwaysBounceHorizontal={false} /> 
